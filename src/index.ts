7e7ea33375c7a93204de48b82deebe8149e3012a
Radu Lucut <lucutradu@gmail.com>
2021-07-13 00:23:18 +0300

    Pass file as an argument

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,53 @@
+import path from 'path';
 import { read, write } from './FileLoader';
 import { parse } from './Parser';
 
 (async function main() {
-  const fileData = await read('example.htnoml');
+  const args = parseArguments();
+  validateFilePath(args.filePath);
 
-  const parseResult = parse(fileData);
+  const fileData = await read(args.filePath);
 
-  write('example.html', parseResult.toHtml());
+  const htmlTree = parse(fileData);
+
+  const destinationPath = getDestinationPath(args.filePath);
+  write(destinationPath, htmlTree.toHtml());
 })();
 
+function getDestinationPath(targetPath: string): string {
+  const extension = path.extname(targetPath);
+  
+  return targetPath.substring(0, targetPath.length - extension.length) + '.html';
+}
+
+function validateFilePath(filePath: string): void {
+  if (!filePath)
+    throw new Error('File must be specified');
+
+  if (path.extname(filePath) !== '.htnoml')
+    throw new Error('File must be of type: ".htnoml"');
+}
+
+function parseArguments(): Arguments {
+  const args: Arguments = {};
+
+  const argv = process.argv;
+  let index = 0;
+  
+  while(index < argv.length) {
+    switch (argv[index]) {
+      case '-f':
+        index++;
+        args.filePath = argv[index];
+        break;
+    }
+    index++;
+  }
+
+  return args;
+}
+
+
+interface Arguments {
+  filePath?: string;
+}
