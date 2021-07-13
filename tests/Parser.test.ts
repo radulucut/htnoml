a9294a9e655b1d722fb76a6079a46c8f55318b71
Radu Lucut <lucutradu@gmail.com>
2021-07-13 23:34:03 +0300

    Update tests

--- tests/Parser.test.ts
+++ tests/Parser.test.ts
@@ -2,37 +2,40 @@ import ElementNode from '../src/ElementNode';
 import TextNode from '../src/TextNode';
 import { parse } from '../src/Parser';
 
-test('parse htnoml', () => {
-  const htnoml = `
+describe('Parser', () => {
+  describe('parse', () => {
+    test('should return html tree', () => {
+      const htnoml = `
 {:html
-	{:body 
-		{class[container main] > This is HTnoML syntax}
-	}
+  {:body 
+    {class[container main] > This is HTnoML syntax}
+  }
 }`;
-  
-  const container = new ElementNode();
-  container.attributes.push({
-    name: 'class',
-    value: 'container main',
+
+      const container = new ElementNode();
+      container.attributes.push({
+        name: 'class',
+        value: 'container main',
+      });
+
+      const textNode = new TextNode();
+      textNode.text =' This is HTnoML syntax';
+      container.childNodes.push(textNode);
+
+      const body = new ElementNode();
+      body.childNodes.push(container);
+      body.nodeTypes.push('body');
+
+      const html = new ElementNode();
+      html.childNodes.push(body);
+      html.nodeTypes.push('html');
+
+      const expectedTree = new ElementNode(true);
+      expectedTree.childNodes.push(html);
+
+      const htmlTree = parse(htnoml);
+
+      expect(htmlTree).toEqual(expectedTree);
+    });
   });
-
-  const textNode = new TextNode();
-  textNode.text =' This is HTnoML syntax';
-  container.childNodes.push(textNode);
-  
-  const body = new ElementNode();
-  body.childNodes.push(container);
-  body.nodeTypes.push('body');
-  
-  const html = new ElementNode();
-  html.childNodes.push(body);
-  html.nodeTypes.push('html');
-
-  const expectedTree = new ElementNode(true);
-  expectedTree.childNodes.push(html);
-
-  const htmlTree = parse(htnoml);
-
-  expect(htmlTree).toEqual(expectedTree);
 });
-
