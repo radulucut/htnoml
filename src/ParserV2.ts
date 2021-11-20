d93c46ed0afb731582de9d5edb7b9a35ff495229
Radu Lucut <lucutradu@gmail.com>
2021-11-20 23:10:34 +0200

    Parse elementType and attributes in V2

--- src/ParserV2.ts
+++ src/ParserV2.ts
@@ -2,51 +2,84 @@ import { TokenStream, Token, TokenType } from './TokenStream';
 
 export function Parser(tokenStream: TokenStream): any {
   function parse() {
-    const root: Node = { type: NodeType.Root, children: [] };
-    let node: Node = root;
+    const childParentMap = new Map();
+    const root: Root = { type: NodeType.Root, childNodes: [] };
+    let node: (Root | Element | Text) = root;
 
     while (tokenStream.peek() !== null) {
       const token = tokenStream.next();
-      console.log('parse: ', token);
 
       if (token.type === TokenType.Whitespace)
         continue;
 
-      if (token.type === TokenType.ReservedChar && token.value === '{') { // TODO: maybe we can have an enum type for this?
-        const newNode: Node = { type: NodeType.Element, children: [] };
+      if (token.type === TokenType.ReservedChar && token.value === ':') {
+        (node as Element).elementType = tokenStream.next().value;
+      }
 
-        node.children.push(newNode);
-        node = newNode;
+      if (token.type === TokenType.Identifier) {
+        const attribute: Attribute = {
+          name: token.value,
+          value: ''
+        };
+
+        readAttribute(attribute);
+
+        (node as Element).attributes.push(attribute);
+      }
+
+      if (token.type === TokenType.ReservedChar && token.value === '{') { // TODO: maybe we can have an enum type for this?
+        const element: Element = { type: NodeType.Element, elementType: 'div', attributes: [], childNodes: [] };
+
+        (node as (Element | Root)).childNodes.push(element);
+
+        childParentMap.set(element, node);
+        node = element;
 
         continue;
+      }
+
+      if (token.type === TokenType.ReservedChar && token.value === '}') {
+        node = childParentMap.get(node);
       }
 
       if (token.type === TokenType.ReservedChar && token.value === '>') {
-        const textNode: Node = { type: NodeType.Text, children: [] };
-        readTextNode(textNode);
+        const textNode: Text = { type: NodeType.Text, text: '' };
+        readText(textNode);
 
-        node.children.push(textNode);
+        (node as Element).childNodes.push(textNode);
 
         continue;
       }
-
-      node.children.push(token);
     } 
 
     return root;
   }
 
-  function readTextNode(node: Node) {
-    let token = tokenStream.next();
+  function readAttribute(attribute: Attribute) {
+    let token = tokenStream.peek();
 
-    while (token !== null) {
-      if (token.type === TokenType.ReservedChar && token.value === '}')
-        return node; 
-
-      node.children.push(token);
-
+    if (token.type === TokenType.ReservedChar && token.value === '[') {
+      tokenStream.next();
       token = tokenStream.next();
+
+      while(token !== null && !(token.type === TokenType.ReservedChar && token.value === ']')) {
+        attribute.value += token.value;
+        token = tokenStream.next();
+      }
     }
+  }
+
+  function readText(textNode: Text) {
+    let token = tokenStream.peek();
+    let text = '';
+
+    while (token !== null && !(token.type === TokenType.ReservedChar && token.value === '}')) {
+      text += tokenStream.next().value;
+
+      token = tokenStream.peek();
+    }
+
+    textNode.text = text;
   }
 
   return { parse };
@@ -54,11 +87,29 @@ export function Parser(tokenStream: TokenStream): any {
 
 interface Node {
   type: NodeType;
-  children: (Node | Token)[]; // TODO: use linked list?
+}
+
+interface Root extends Node {
+  childNodes: (Element | Text)[]; // TODO: use linked list?
+}
+
+interface Element extends Node {
+  elementType: string;
+  attributes: Attribute[];
+  childNodes: (Element | Text)[]; // TODO: use linked list?
+}
+
+interface Text extends Node {
+  text: string;
+}
+
+interface Attribute {
+  name: string;
+  value: string;
 }
 
 const enum NodeType {
   Root = 1,
-  Element = 2,
-  Text = 3,
+    Element = 2,
+    Text = 3,
 }
